1396fa6da9ef96992a66b98f82e2f7058ec076b4
howItPhills <vlad-man001@mail.ru>
2022-01-09 13:48:53 +0300

    added some styles to directory menu

--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, size, imageUrl }) => {
    return (
-      <div className='menu-item'>
+      <div className={`${size} menu-item`}>
+         <div
+            className='background-image'
+            style={{
+               background: `url(${imageUrl}) center/cover`
+            }} />
          <div className="content">
             <h1 className="title">{title}</h1>
             <span className="subtitle">shop now</span>
