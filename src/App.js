5b30d191ea03438af5383aadc6a43fe7dfeef80c
howItPhills <vlad-man001@mail.ru>
2022-01-12 15:14:36 +0300

    added redux and made user-reducer + updated App and Header components to work with redux

--- src/App.js
+++ src/App.js
@@ -7,34 +7,27 @@ import Header from './components/header/Header.component';
 import SignInPage from './pages/signInPage/SignInPage.component';
 import ContactPage from './pages/contactPage/ContactPage.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { connect } from 'react-redux'
+import { setCurrentUserAC } from './redux/user/user.actions';
+
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      currentUser: null,
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth); //getting userRef from createUserProfileDocument's return
         userRef.onSnapshot(snapShot => { //onSnapShot() = refObj.get() - getting snapshotObject (for info)
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(), // spreading the rest of data (email, displayName, etc.) 
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(), // spreading the rest of data (email, displayName, etc.) 
           })
         }
         )
-      } else this.setState({ currentUser: null })
-
+      } else setCurrentUser(userAuth)
     })
   }
 
@@ -46,7 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -58,4 +51,9 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUserAC(user))
+})
+
+
+export default connect(null, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,18 +16,17 @@ firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
    if (!userAuth) return;
-   //reference object can be changed by CRUD, snapshot objects - dont (only for checking existance of ref object in data-base)
 
+   //reference object can be changed by CRUD, snapshot objects - dont (only for checking existance of ref object in data-base)
    const userRef = firestore.doc(`users/${userAuth.uid}`) //documentRef - object, used for CRUD - operations (get, set, delete, update)
 
    const snapShot = await userRef.get() // documentSnapshot - object, used only to display information about ref object (mostly for checking existance in data-base) 
 
    if (!snapShot.exists) { //checking existance of our documentRef object. If it doesnt - set information inside our ref object in data-base
-      const { displayName, email } = userAuth;
+      const { email } = userAuth;
       const createdAt = new Date();
       try {
          await userRef.set({
-            displayName,
             email,
             createdAt,
             ...additionalData
