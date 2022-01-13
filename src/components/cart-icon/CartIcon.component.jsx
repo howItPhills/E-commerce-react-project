6d389cfb4fd58fe6299ea94d34d76d423d159bd8
howItPhills <vlad-man001@mail.ru>
2022-01-13 18:07:00 +0300

    used reselect library + made selectors for CartDropdown component and CartIcon component - helps to stop rerendering componentsif it isnt needed

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -3,16 +3,21 @@ import { ReactComponent as CartImage } from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 import { connect } from 'react-redux';
 import { toggleCartDropdownAC } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({ toggleCartDropdown }) => (
+const CartIcon = ({ toggleCartDropdown, itemCount }) => (
    <div className='cart-icon' onClick={toggleCartDropdown}>
       <CartImage className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{itemCount}</span>
    </div>
 )
+
+const mapStateToProps = state => {
+   return { itemCount: selectCartItemsCount(state) }
+}
 
 const mapDispatchToProps = dispatch => ({
    toggleCartDropdown: () => dispatch(toggleCartDropdownAC())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
